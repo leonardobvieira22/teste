10bbbc0e200ac2622820bc08b56a7f045858d9ae
leonardobvieira22 <150559161+leonardobvieira22@users.noreply.github.com>
2023-11-20 22:43:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+/*!
+* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+const form = document.getElementById('formLogin');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  // fazer requisição AJAX para valida_login.php
+  
+  this.submit(); // submete o formulário após AJAX
+});
+
+
+const form = document.querySelector('#formPesquisa');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  // faz busca
+  alert('Pesquisando...');
+});
